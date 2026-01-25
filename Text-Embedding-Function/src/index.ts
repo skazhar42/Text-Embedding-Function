cbe56a8cefe0a8b8866f7235bedcdd3e290fb6b5
AZHARUDDIN SHAIK <azharuddinshaik@AZHARUDDINs-MacBook-Pro.local>
2026-01-25 20:11:07 +0800

    fix: ts build

--- Text-Embedding-Function/src/index.ts
+++ Text-Embedding-Function/src/index.ts
@@ -1,3 +1,5 @@
+
+
 export function greet(name: string): string {
   return `Hello, ${name}!`
 }
--- Text-Embedding-Function/vite.config.ts
+++ Text-Embedding-Function/vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path'
 
 export default defineConfig({
   build: {
+    emptyOutDir: false,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'TextEmbeddingLib',
